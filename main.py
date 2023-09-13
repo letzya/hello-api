82041f3c2991887fb63575a789ed62aacb2870ff
lghiur <laurentiu.ghiur@gmail.com>
2023-09-13 09:12:09 +0300

    Committing changes to master

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class HelloRequest(BaseModel):
     entity: str
 
 # GET request handler
-@app.get("/internal", tags=[])
+@app.get("/internal", tags=[internal])
 async def read_hello():
     return {"message": "This is internal Endpoint"}
 
