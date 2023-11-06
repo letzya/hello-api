7bd5d60d2cb1bf3fb4b672c288e9ff345213b53d
lghiur <laurentiu.ghiur@gmail.com>
2023-11-06 15:50:03 +0200

    Committing changes to master

--- api-one.py
+++ api-one.py
@@ -18,7 +18,7 @@ class HelloRequest(BaseModel):
     entity: str
 
 # GET request handler
-@app.get("/internal", tags=[internal])
+@app.get("/internal", tags=[])
 async def read_hello():
     return {"message": "This is internal Endpoint"}
 
@@ -71,7 +71,7 @@ app.openapi = custom_openapi
 
 def generate_openapi_json():
     openapi_schema = app.openapi()
-    with open("../oas/my-api-v2.oas.json", "w") as file:
+    with open("../oas/my-api.oas.json", "w") as file:
         json.dump(openapi_schema, file, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
